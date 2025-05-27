d2f4d14c6caad09c8305688591dcec598f23a209
Midlay <stasyukartem02@gmail.com>
2025-05-27 10:53:12 +0300

    update for railwayн

--- src/main/java/com/example/bot/GiftBot.java
+++ src/main/java/com/example/bot/GiftBot.java
@@ -36,6 +36,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class GiftBot extends TelegramLongPollingBot {
